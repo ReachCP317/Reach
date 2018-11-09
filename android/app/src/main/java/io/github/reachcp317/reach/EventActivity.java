1c664fcf35ad7adbaa48ea84cad9062f9935bd6a
Dustin <36052980+dsdugal@users.noreply.github.com>
2018-11-09 18:04:33 -0500

    Skeleton code for classes.

---
+++ android/app/src/main/java/io/github/reachcp317/reach/EventActivity.java
@@ -0,0 +1,52 @@
+package io.github.reachcp317.reach;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/* TO DO:
+   need to import event class
+*/
+
+public class EventActivity extends FragmentActivity {
+
+    private Event currentEvent;
+
+    void interested() {
+        /*
+            Called when the user presses the 'Interested' button. Increments the Event's
+            "totalInterested" value by 1. When the button is pressed again, decrement by 1.
+        */
+    }
+
+    void setEvent(Event event) {
+        // ???
+    }
+
+    void deleteEvent() {
+        /*
+            Called when the 'Delete Event' button is pressed. Clears all event information and
+            removes the event from the database.
+        */
+    }
+
+    void updateEvent() {
+        /*
+            Submits changes to the event's information to the database.
+        */
+    }
+
+    void changeLocation() {
+        /*
+            Opens MainActivity and makes the event's Marker draggable. Once the marker has been
+            confirmed to be in the correct position, opens the EventView.
+        */
+    }
+
+    void giveRating(float rating) {
+        /*
+            Called when the user presses the 'Rate' button. Calculates the new rating of the
+            event's creator and updates it to the database.
+        */
+    }
+}
--- android/app/src/main/java/io/github/reachcp317/reach/MainActivity.java
+++ android/app/src/main/java/io/github/reachcp317/reach/MainActivity.java
@@ -1,13 +1,67 @@
 package io.github.reachcp317.reach;
 
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+/* TO DO:
+    need to import event class
+    need to import googlemap class
+    need to import marker class
+*/
+
+public class MainActivity extends FragmentActivity {
+
+    private ArrayList<Event> eventsList;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    @Override
+    protected void onMapReady(GoogleMap mMap) {
+        // updateEventsList(), setMyLocationEnabled(
+    }
+    */
+
+    protected boolean onMarkerClick(Marker marker) {
+        /*
+            Called when a marker is clicked/tapped. This will be overridden to go to the
+            EventActivity.
+        */
+    }
+
+    protected void onMarkerDragEnd(Marker marker) {
+        //
+    }
+
+    public void updateEventsList() {
+        /*
+            Iterate through the list of events in the database, storing only nearby locations
+            (small difference in lat/long) into eventsList. For each event, if distanceTo(Location)
+            returns a value (in meters, convert to km) less than or equal to the user's radius, a
+            Marker will be placed via addMarker().
+        */
+    }
+
+    public void setRadius() {
+        /*
+            Sets the user's radius to whatever value in kilometers was entered in the text box.
+        */
+    }
+
+    public void createEvent() {
+        /*
+            Called when the 'Create Event' button is pressed. Places a draggable marker on the
+            user's location. Once the marker has been confirmed to be in the correct position,
+            opens the EventView.
+        */
     }
 }
