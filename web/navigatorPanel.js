fa88f07e91e0da566794298636e22cf7cf5b25c0
crushelves5 <37851515+crushelves5@users.noreply.github.com>
2018-11-26 14:01:51 -0500

    Page Layout updates

---
+++ web/navigatorPanel.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("navigatorPanel").style.width = "200px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("navigatorPanel").style.width = "0";
+}
