e8d11a8f1aa8d77770cc2d7459235d98b4c453ab
Dustin <36052980+dsdugal@users.noreply.github.com>
2018-11-28 20:34:27 -0500

    Update User.java
    Added radius field, and setter/getter methods for it.

--- android/app/src/main/java/io/github/reachcp317/reach/User.java
+++ android/app/src/main/java/io/github/reachcp317/reach/User.java
@@ -6,12 +6,14 @@ import java.time.LocalDate
 public class User {
 	
 	private int id;
+	private float radius;
 	private float rating;
 	private String name;
 	private Date date_of_birth;
 
-	public User(String name, Date date_of_birth) {
+	public User(String name, Date date_of_birth, float radius) {
 		// this.id = // needs to be assigned by the application/database
+		this.radius = radius;
 		this.rating = 0.0;
 		this.name = name;
 		this.date_of_birth = date_of_birth;
@@ -19,6 +21,10 @@ public class User {
 	
 	public int getID() {
 		return this.id;
+	}
+	
+	public float getRadius() {
+		return this.radius;
 	}
 	
 	public float getRating() {
@@ -29,6 +35,10 @@ public class User {
 		LocalDate birthDate = this.date_of_birth.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 		LocalDate currentDate = LocalDate.now();
         return Period.between(birthDate, currentDate).getYears();		
+	}
+	
+	public void setRadius(float radius) {
+		this.radius = radius;
 	}
 	
 	public void setRating(float rating) {
