30d2de9b6dd412499ff33520ec72555a46fcf4e4
Mike Pintur <33645637+pint5070@users.noreply.github.com>
2018-11-30 11:07:58 -0500

    Add files via upload
    Updates DBConnect.java to include queryEvent method which given an eventId will pull information from an event_user view to collect more event details.

--- Database/Java Connection/src/DBConnect.java
+++ Database/Java Connection/src/DBConnect.java
@@ -34,122 +34,9 @@ public class DBConnect {
 	}
 	
 	
-	/**
-	 * 
-	 * Method will hash the given password to SHA-256 then will compare hash codes with the database to verify the passwords are correct.
-	 * 
-	 * @param email
-	 * @param password 
-	 * @return -1 if no result set 0- if hash code does not match database hash code 1- if hash codes match
-	 */
-	public int passwordChecker(String email, String password) {
-		MessageDigest mDigest;
-		String hashCode ="";
-		StringBuffer sb = new StringBuffer();
-		try {
-			mDigest = MessageDigest.getInstance("SHA-256");
-			byte[] result = mDigest.digest(password.getBytes());
-			for (int i = 0; i < result.length; i++) {
-				sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-			}
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		}
-		
-		String query = String.format("SELECT pwd FROM user WHERE email = '%s'", email);
-		try {
-			rs= st.executeQuery(query);
-			if (rs.next()) {
-				hashCode = rs.getString("pwd");
-			} else {
-				return -1;
-			}
-		} catch (Exception ex) {
-			System.out.println("Error: "+ ex);
-		}
+	
+	
 
-		if (hashCode.equals(sb.toString())) {
-			return 1;
-		}
-		
-		return 0;	
-	}
-	
-	
-	
-	/*
-	 * Inserts a user into the user database table. The database automatically encrypts the password to 
-	 * SHA2 and auto generates userID
-	 * 
-	 * @param email - WLU email of the user 
-	 * @param fName - first name of the user
-	 * @param lName - last name of the user
-	 * @param password - password that the user wishes to create account with
-	 * 
-	 */
-	public void createUser(String email, String fName, String lName, String password) {
-		String name = fName + " "+ lName;
-		String insertUser;
-		insertUser = String.format("INSERT INTO user (email, pwd, name) VALUES ('%s','%s','%s')", email, password, name );
-		try {
-			st.execute(insertUser);
-		} catch(Exception ex) {
-			System.out.println("Error: "+ ex);
-
-		}
-	}
-	/*
-	 * createEvent into event table
-	 * 
-	 * event table fields -
-	 * 
-	 * eventID, hostID, address, description, latitude, longitude, startDate, endDate
-	 * 
-	 * @param email - host email
-	 * @param desc - description of event
-	 * @param lon - longitude coordinate
-	 * @param lat - latitude coordinate
-	 * @param start - start date of the event
-	 * @param end - end date of the event
-	 * @param address - address of the event
-	 */
-	public void createEvent(String email, String desc, int lon, int lat, String start, String end, String address ) {
-		int userID = getUserID(email);
-		String insertEvent;
-		//insertEvent = String.format("INSERT INTO event (eventID, desc, longitude, latitude, startDate, endDate, address)"
-		//		+ " VALUES (%d, '%s', %d, %d, '%s', '%s', '%s')", userID, desc, lon, lat, start, end, address);
-		insertEvent = String.format("INSERT INTO event (hostID, address, description, latitude, longitude, startDate, endDate)"
-				+ " VALUES (%d, '%s', '%s', %d, %d, '%s', '%s')", userID, "address", desc,lat, lon, start, end);
-		System.out.println(insertEvent);
-		try {
-			st.execute(insertEvent);
-		}catch (Exception ex) {
-			System.out.println("Error: "+ ex);
-		}
-	}
-	
-	
-	/**
-	 * fetches userID from user table when given email as a parameter
-	 * @param email - user email
-	 * @return - returns the unique userID from the given email address.
-	 */
-	public int getUserID(String email) {
-		int userID = 0;
-		String query = String.format("SELECT userID FROM user"
-				+ " WHERE email = '%s'", email);
-		try {
-			rs = st.executeQuery(query);
-			while(rs.next()) {
-				userID = rs.getInt("userID");
-			}
-		} catch (Exception ex) {
-			System.out.println("Error: " + ex);
-		}
-		
-			return userID;
-	}
-	
 	/**
 	 * 
 	 * Returns an ArrayList of EventIds that are within a condition of the users current Latitude and Longitude. 
@@ -204,6 +91,159 @@ public class DBConnect {
 		return events;
 		
 	}
+	
+	
+	/*
+	 * Inserts a user into the user database table. The database automatically encrypts the password to 
+	 * SHA2 and auto generates userID
+	 * 
+	 * @param email - WLU email of the user 
+	 * @param fName - first name of the user
+	 * @param lName - last name of the user
+	 * @param password - password that the user wishes to create account with
+	 * 
+	 */
+	public void createUser(String email, String fName, String lName, String password) {
+		String name = fName + " "+ lName;
+		String insertUser;
+		insertUser = String.format("INSERT INTO user (email, pwd, name) VALUES ('%s','%s','%s')", email, password, name );
+		try {
+			st.execute(insertUser);
+		} catch(Exception ex) {
+			System.out.println("Error: "+ ex);
+
+		}
+	}
+	
+	
+	/*
+	 * createEvent into event table
+	 * 
+	 * event table fields -
+	 * 
+	 * eventID, hostID, address, description, latitude, longitude, startDate, endDate
+	 * 
+	 * @param email - host email
+	 * @param desc - description of event
+	 * @param lon - longitude coordinate
+	 * @param lat - latitude coordinate
+	 * @param start - start date of the event
+	 * @param end - end date of the event
+	 * @param address - address of the event
+	 */
+	public void createEvent(String email, String desc, int lon, int lat, String start, String end, String address ) {
+		int userID = getUserID(email);
+		String insertEvent;
+		//insertEvent = String.format("INSERT INTO event (eventID, desc, longitude, latitude, startDate, endDate, address)"
+		//		+ " VALUES (%d, '%s', %d, %d, '%s', '%s', '%s')", userID, desc, lon, lat, start, end, address);
+		insertEvent = String.format("INSERT INTO event (hostID, address, description, latitude, longitude, startDate, endDate)"
+				+ " VALUES (%d, '%s', '%s', %d, %d, '%s', '%s')", userID, "address", desc,lat, lon, start, end);
+		System.out.println(insertEvent);
+		try {
+			st.execute(insertEvent);
+		}catch (Exception ex) {
+			System.out.println("Error: "+ ex);
+		}
+	}
+	
+	
+	/**
+	 * Returns a result set from the database view v_event_name when given an event ID.
+	 * @param eventID - Possibly from an ArrayList of event ID's
+	 */
+	public void queryEvent(int eventID) {
+		String query = null;
+		query = String.format("SELECT * FROM v_event_name WHERE eventID = %d",eventID);
+		int lon = 0;
+		int lat = 0;
+		String hostName = null;
+		String address;
+		String startDate;
+		String endDate;
+		
+		
+		
+		try {
+			rs = st.executeQuery(query);
+			while (rs.next()) {
+				lon = rs.getInt("longitude");
+				lat = rs.getInt("latitude");
+				hostName = rs.getString("hostName");
+				address = rs.getString("address");
+				startDate = rs.getString("startDate");
+				endDate = rs.getString("endDate");
+			}
+			System.out.println(eventID + " " + hostName);
+		} catch (Exception ex) {
+			System.out.println("Error: "+ ex);
+		}
+		
+	}
+
+	
+	
+	/**
+	 * fetches userID from user table when given email as a parameter
+	 * @param email - user email
+	 * @return - returns the unique userID from the given email address.
+	 */
+	public int getUserID(String email) {
+		int userID = 0;
+		String query = String.format("SELECT userID FROM user"
+				+ " WHERE email = '%s'", email);
+		try {
+			rs = st.executeQuery(query);
+			while(rs.next()) {
+				userID = rs.getInt("userID");
+			}
+		} catch (Exception ex) {
+			System.out.println("Error: " + ex);
+		}
+		
+			return userID;
+	}
+	
+	/**
+	 * 
+	 * Method will hash the given password to SHA-256 then will compare hash codes with the database to verify the passwords are correct.
+	 * 
+	 * @param email
+	 * @param password 
+	 * @return -1 if no result set 0- if hash code does not match database hash code 1- if hash codes match
+	 */
+	public int passwordChecker(String email, String password) {
+		MessageDigest mDigest;
+		String hashCode ="";
+		StringBuffer sb = new StringBuffer();
+		try {
+			mDigest = MessageDigest.getInstance("SHA-256");
+			byte[] result = mDigest.digest(password.getBytes());
+			for (int i = 0; i < result.length; i++) {
+				sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+			}
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		String query = String.format("SELECT pwd FROM user WHERE email = '%s'", email);
+		try {
+			rs= st.executeQuery(query);
+			if (rs.next()) {
+				hashCode = rs.getString("pwd");
+			} else {
+				return -1;
+			}
+		} catch (Exception ex) {
+			System.out.println("Error: "+ ex);
+		}
+
+		if (hashCode.equals(sb.toString())) {
+			return 1;
+		}
+		
+		return 0;	
+	}
+	
 	
   }
 
