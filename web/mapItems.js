356d5a6b24047139e33674af364a3c8edfcbabdd
zvanderburgt <zvanderburgt@gmail.com>
2018-12-01 00:00:02 -0500

    added a second marker to dashbaord and more UI tweeks

--- web/mapItems.js
+++ web/mapItems.js
@@ -1,8 +1,8 @@
 var slider = document.getElementById("myRadius");
 var output = document.getElementById("radius");
-output.innerHTML = slider.value+ " km"; // Display the default slider value
-
+output.innerHTML = slider.value + " km";
+// Display the default slider value
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-    output.innerHTML = this.value + " km";
+  output.innerHTML = this.value + " km";
 }
