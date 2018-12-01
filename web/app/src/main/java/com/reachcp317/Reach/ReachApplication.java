d608e6162ef7449640cf05149b90a4d3a18ea590
MorgenneB <morgenne.b@gmail.com>
2018-12-01 12:06:23 -0500

    Add files via upload

---
+++ web/app/src/main/java/com/reachcp317/Reach/ReachApplication.java
@@ -0,0 +1,12 @@
+package com.reachcp317.Reach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReachApplication.class, args);
+	}
+}
