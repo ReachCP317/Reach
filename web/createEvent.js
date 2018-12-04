9ec2cab3f90aa6f02c3c46699a4d6e659c622ca9
Jamie-Omorodion <43283715+Jamie-Omorodion@users.noreply.github.com>
2018-12-04 12:08:25 -0500

    Add files via upload

---
+++ web/createEvent.js
@@ -0,0 +1,17 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("navigatorPanel").style.width = "20%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("navigatorPanel").style.width = "0%";
+}
+
+function pageSettings() {
+  var title = document.title;
+  if (title == "Dashboard") {
+    document.getElementById("search").href = "#";
+    document.getElementById("search").style.color = "#FF6E83";
+  }
+}
