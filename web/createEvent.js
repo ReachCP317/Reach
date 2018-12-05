86af246eb0b90a40d638907f7ea12ed1d3f8017c
Jamie-Omorodion <43283715+Jamie-Omorodion@users.noreply.github.com>
2018-12-05 02:35:06 -0500

    Update createEvent.js

--- web/createEvent.js
+++ web/createEvent.js
@@ -15,3 +15,39 @@ function pageSettings() {
     document.getElementById("search").style.color = "#FF6E83";
   }
 }
+
+function eventSubmit(){
+  var location = document.getElementById("eventLocation").value;
+  var range = document.getElementById("eventRange").value;
+  var name = document.getElementById("eventName").value;
+  var startDateTime = document.getElementById("eventStartDate").value;
+  var endDateTime = document.getElementById("eventEndDate").value;
+  var capacity = document.getElementById("eventCapacity").value;
+  var type = document.getElementById("eventType").value;
+  var numDays = document.getElementById("eventDays").value;
+  var description = document.getElementById("eventDescription").value;
+  var userID = document.getElementById("userID").value;
+
+  var errMessage = "";
+  var returnCode = createEvent(location,range,name,startDateTime,endDateTime,
+    capacity,type,numDays,description);
+
+  if (returnCode != 0){
+    switch(result){
+      case -1:
+        /*unknown error; we need to change the error message so that it tells them what to do lol*/
+        errMessage = "An unknown error has occurred.";
+      case -2:
+        /*invalid userID*/
+      case -3:
+        /**/
+      case -4:
+        /**/
+      case -5:
+        /**/
+    }
+  }
+
+
+  document.getElementById("errMessage").value = errMessage;
+}
