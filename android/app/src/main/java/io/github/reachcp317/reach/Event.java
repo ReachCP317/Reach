fbd8e6f474c0756698cf09390eb6fca53a4516fd
Joshua Varga <jpewpoopachew@gmail.com>
2018-12-05 10:35:21 -0500

    :memo: Add Comments

--- android/app/src/main/java/io/github/reachcp317/reach/Event.java
+++ android/app/src/main/java/io/github/reachcp317/reach/Event.java
@@ -1,9 +1,15 @@
 package io.github.reachcp317.reach;
 
+ // import statements
 import android.location.Location;
 
 import java.util.Date;
 
+/**
+ * @author  Joshua Varga
+ * @version 0.1
+ * @since   0.1
+ */
 public class Event {
     private String name;
     private String description;
@@ -24,60 +30,132 @@ public class Event {
         this.totalInterested = 0;
     }
 
-    //Getters
+    // Getters
+
+    /**
+     * Gets the name of the event.
+     *
+     * @return The name of the event.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Gets the description of the event.
+     *
+     * @return The description of the event
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Gets the address of the event.
+     *
+     * @return The address of the event.
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     * Gets the location of the event.
+     *
+     * @return The location of the event.
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     * Gets the starting time of the event.
+     *
+     * @return The starting time of the event.
+     */
     public Date getStartTime() {
         return startTime;
     }
 
+    /**
+     * Gets the ending time of the event.
+     *
+     * @return The ending time of the event.
+     */
     public Date getEndTime() {
         return endTime;
     }
 
+    /**
+     * Gets the total amount of users interested in the event.
+     *
+     * @return The total amount of users interested in the event.
+     */
     public int getTotalInterested() {
         return totalInterested;
     }
 
-    //Setters
+    // Setters
+
+    /**
+     * Sets the name of the event.
+     *
+     * @param name The new name of the event.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the description of the event.
+     *
+     * @param description the new description of the event.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Sets the address of the event.
+     *
+     * @param address the new address of the event.
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     * Sets the location of the event.
+     *
+     * @param location The new location of the event.
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     * Sets the starting time of the event.
+     *
+     * @param startTime The new starting time of the event.
+     */
     public void setStartTime(Date startTime) {
         this.startTime = startTime;
     }
 
+    /**
+     * Sets the ending time of the event.
+     *
+     * @param endTime The new ending time of the event.
+     */
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
 
+    /**
+     * Sets the total amount of users interested in the event.
+     *
+     * @param totalInterested The new total amount of users interested in the event.
+     */
     public void setTotalInterested(int totalInterested) {
         this.totalInterested = totalInterested;
     }
