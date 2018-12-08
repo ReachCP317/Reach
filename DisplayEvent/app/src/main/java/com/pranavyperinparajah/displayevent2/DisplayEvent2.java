dcced0b5cafecae0936851f6a48f00721d4e5d36
midusan <midusan54@gmail.com>
2018-12-08 14:34:51 -0500

    Merge

--- DisplayEvent/app/src/main/java/com/pranavyperinparajah/displayevent2/DisplayEvent2.java
+++ DisplayEvent/app/src/main/java/com/pranavyperinparajah/displayevent2/DisplayEvent2.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class DisplayEvent2 extends AppCompatActivity {
+public class DisplayEvent extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Maps_page/app/src/main/java/com/reach/maps_page/FindEvents.java
+++
@@ -1,5 +0,0 @@
-package com.reach.maps_page;
-//For Database connection (Display list of 5 events nearby)
-public class FindEvents {
-
-}
